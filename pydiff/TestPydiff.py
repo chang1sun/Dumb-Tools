2bfcdc313a4da6d32e0f8e3afc4b06364411cc20
CapChang <ChangNoob@outlook.com>
2020-08-08 18:38:58 +0800

    Add unittest

---
+++ pydiff/TestPydiff.py
@@ -0,0 +1,7 @@
+from pydiff import Pydiff
+
+py = Pydiff()
+d1, d2 = r"D://Test//testdir1", r"D://Test//testdir2"
+if py.has_diffs(d1, d2):
+    py.log()
+
--- pydiff/main.py
+++
@@ -1,7 +0,0 @@
-from pydiff import Diff
-
-py = Diff()
-d1, d2 = r"D://Test//testdir1", r"D://Test//testdir2"
-if py.has_diffs(d1, d2):
-    py.log()
-

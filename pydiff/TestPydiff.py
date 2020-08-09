23f15a1e7ae68adce255859ae885829908bf1c8e
CapChang <ChangNoob@outlook.com>
2020-08-09 22:23:47 +0800

    unittest

--- pydiff/TestPydiff.py
+++ pydiff/TestPydiff.py
@@ -1,7 +1,111 @@
-from pydiff import Pydiff
+import unittest
+from os import path
+from pydiff import PyDiff, CanNotFindPathError, NotTheRightTypeError
 
-py = Pydiff()
-d1, d2 = r"D://Test//testdir1", r"D://Test//testdir2"
-if py.has_diffs(d1, d2):
-    py.log()
+class TestPyDiff(unittest.TestCase):
 
+    def try_has_diff(self, d1, d2, result=None, ignore_space=True):
+        pf = PyDiff()
+        pf.has_diff(d1, d2, ignore_space=ignore_space)
+        if result:
+            self.assertEqual(''.join(pf._log_output), result)
+
+    def try_has_diffs(self, d1, d2, result=None, ignore_space=True):
+        pf = PyDiff()
+        pf.has_diffs(d1, d2, ignore_space=ignore_space)
+        if result:
+            self.assertEqual(''.join(pf._log_output), result)
+
+    def test_ignore_space_true(self):
+        d1 = r'D://Test//t3.txt'
+        d2 = r'D://Test//t4.txt'
+        self.try_has_diff(d1, d2, result=PyDiff._NO_DIFF)
+
+    def test_with_space_False(self):
+        d1 = r'D:\Test\t3.txt'
+        d2 = r'D:\Test\t4.txt'
+        res = r"D://Test//t3.txt: line 1, I <sp> have <sp> a <sp> space <sp>  <sp>  <sp>  <sp>  <sp>  <sp>  <sp> "\
+              + '\n' + r'D://Test//t4.txt: line 1, I <sp> have <sp> a <sp> space' + '\n\n'
+
+        self.try_has_diff(d1, d2, result=PyDiff._FILE_DIFF+res, ignore_space=False)
+
+    def test_ignore_space_false(self):
+        d1 = r'D:\Test\t5.txt'
+        d2 = r'D:\Test\t6.txt'
+        res_list = [
+            PyDiff._FILE_DIFF,
+            r'D://Test//t5.txt: line 1, Excellent works,',
+            '\n',
+            r'D://Test//t6.txt: line 1, Excellent work,',
+            '\n\n\n',
+            r'D://Test//t5.txt: line 2, good job Chang!',
+            '\n',
+            r'D://Test//t6.txt: line 2, but still need improvement!',
+            '\n\n\n'
+        ]
+        self.try_has_diff(d1, d2, result=''.join(res_list), ignore_space=True)
+
+    def test_CanNotFindPathError(self):
+        d1 = r'D:\Test\t100.txt'
+        d2 = r'D:\Test\t6.txt'
+
+        with self.assertRaises(CanNotFindPathError):
+            self.try_has_diff(d1, d2)
+
+    def test_NotTheRightTypeError(self):
+        d1 = r'D:\Test\t5.txt'
+        d2 = r'D:\Test\t6.txt'
+
+        with self.assertRaises(NotTheRightTypeError):
+            self.try_has_diffs(d1, d2)
+
+    def test_empty_folders(self):
+        d1 = r'D:\Test\t1'
+        d2 = r'D:\Test\t2'
+        self.try_has_diffs(d1, d2, result=PyDiff._NO_DIFF)
+
+    def test_folders_true(self):
+        d1 = r'D:\Test\testdir3'
+        d2 = r'D:\Test\testdir4'
+        self.try_has_diffs(d1, d2, result=PyDiff._NO_DIFF, ignore_space=True)
+
+    def test_folders_false(self):
+        d1 = r'D:\Test\testdir1'
+        d2 = r'D:\Test\testdir2'
+
+        res_list_ordered = [
+            PyDiff._NAME_DIFF,
+            r'Only in D:\Test\testdir1:',
+            '\n',
+            r'//sample3.txt',
+            '\n\n'
+            r'No extra files in D:\Test\testdir2',
+            '\n\n',
+            PyDiff._FILE_DIFF,
+        ]
+
+        res_list_not_ordered = [
+            r'D://Test//testdir1//ssps//ggpa//sample1.txt: line 1, !'
+            + '\n'
+            + r'D://Test//testdir2//ssps//ggpa//sample1.txt: line 1, So what?'
+            + '\n\n\n',
+
+            r'D://Test//testdir1//ssps//ggpa//sample1.txt: line 2, Changed?'
+            + '\n'
+            + r'D://Test//testdir2//ssps//ggpa//sample1.txt: line 2, I change my mind'
+            + '\n\n\n',
+
+            r'D://Test//testdir1//ssps//ggpd//sample2.txt: line 1, NOthing!'
+            + '\n'
+            + r'D://Test//testdir2//ssps//ggpd//sample2.txt: line 1, not a shit'
+            + '\n\n\n'
+        ]
+        pf = PyDiff()
+        pf.has_diffs(d1, d2, ignore_space=True)
+        self.assertEqual(''.join(pf._log_output[:5]), ''.join(res_list_ordered))
+        for elem in res_list_not_ordered:
+            self.assertIn(elem, pf._log_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- pydiff/pydiff.py
+++ pydiff/pydiff.py
@@ -1,5 +1,4 @@
-# by ChangNoob on 2020-07-22
-# works fine till 2020-07-25
+# by changnoob
 
 """
 I found a useful tool named 'diffmerge', which implement swift function to find difference between
@@ -13,11 +12,12 @@ import os
 import datetime
 
 
-class Diff:
+class PyDiff(object):
 
-    DATETIME = datetime.datetime.now().__format__("[%Y-%m-%d %H:%M:%S]")
-    NAME_DIFF = f'{DATETIME}[Info]: Include different files:\n\n'
-    FILE_DIFF = f'{DATETIME}[Info]: Find Files differences exist in:\n\n'
+    _DATETIME = datetime.datetime.now().__format__("[%Y-%m-%d %H:%M:%S]")
+    _NAME_DIFF = f'{_DATETIME}[Info]: Include different files:\n\n'
+    _FILE_DIFF = f'{_DATETIME}[Info]: Find Files differences exist in:\n\n'
+    _NO_DIFF = f'{_DATETIME}[Info]: No difference exist\n'
 
     def __init__(self):
         """
@@ -28,6 +28,9 @@ class Diff:
         self._log_output = []
         self._cache = []
 
+    def __repr__(self):
+        return "<PyDiff object>"
+
     def log(self):
         """
         print the latest log
@@ -35,18 +38,12 @@ class Diff:
         """
         try:
             assert self._log_output
-            for output in self._log_output:
-                print(output, end='')
+            print(''.join(self._log_output))
         except AssertionError:
-            print(f"{self.DATETIME}[Info]: No latest log!")
+            print(f"{self._DATETIME}[Info]: No latest log!")
 
     def _find(self, d1, d2, ignore_space) -> bool:
-        """
-        :param d1: Absolute path of first file
-        :param d2: Absolute path of second file
-        :param ignore_space: set True to ignore the space at the end of lines, default True
-        :return: True if any difference exist, False otherwise.
-        """
+        d1, d2 = d1.replace("\\", '//'), d2.replace("\\", '//')
         with open(d1) as f1, open(d2) as f2:
             lines1 = [line for line in f1]
             lines2 = [line for line in f2]
@@ -56,8 +53,8 @@ class Diff:
                 lines1.extend(['------Empty line------!' for i in range(len(lines2) - len(lines1))])
             for i, (l1, l2) in enumerate(zip(lines1, lines2), start=1):
 
-                l1_f = l1.rstrip() + '\n' if ignore_space else l1 + '\n'
-                l2_f = l2.rstrip() + '\n' if ignore_space else l2 + '\n'
+                l1_f = l1.rstrip() + '\n' if ignore_space else l1.replace(' ', ' <sp> ')
+                l2_f = l2.rstrip() + '\n' if ignore_space else l2.replace(' ', ' <sp> ')
                 if l1_f != l2_f:
                     self._cache.append(f"{d1}: line {i}, {l1_f}{d2}: line {i}, {l2_f}\n\n")
 
@@ -80,11 +77,18 @@ class Diff:
         :param ignore_space: set True to ignore the space at the end of lines, default True
         :return: True if any difference exist, False otherwise.
         """
+        if not os.path.exists(d1) or not os.path.exists(d2):
+            raise CanNotFindPathError("Paths don't exist")
+
+        if os.path.isdir(d1) or os.path.isdir(d2):
+            raise NotTheRightTypeError("The paths should be files")
+
         if self._find(d1, d2, ignore_space):
-            self._log_output.append(self.FILE_DIFF)
+            self._log_output.append(self._FILE_DIFF)
             self._log_output.extend(self._cache)
             self._cache = []
             return True
+        self._log_output.append(self._NO_DIFF)
         return False
 
     def has_diffs(self, d1, d2, ignore_space=True) -> bool:
@@ -95,20 +99,25 @@ class Diff:
         :param ignore_space: set True to ignore the space at the end of lines, default True
         :return: True if any difference exist, False otherwise.
         """
+        if not os.path.exists(d1) or not os.path.exists(d2):
+            raise CanNotFindPathError("Paths don't exist")
+
+        if os.path.isfile(d1) or os.path.isfile(d2):
+            raise NotTheRightTypeError("The paths should be folders")
+
         if self._finds(d1, d2, ignore_space=ignore_space):
             if self._cache:
-                self._log_output.append(self.FILE_DIFF)
+                self._log_output.append(self._FILE_DIFF)
                 self._log_output.extend(self._cache)
                 self._cache = []
             return True
+        self._log_output.append(self._NO_DIFF)
         return False
 
     @staticmethod
     def _iterate(abs_d) -> set:
         """
         Use dfs to walk through every file and record their relative path
-        :param abs_d: Absolute path of the directory
-        :return: file set(in relative path way)
         """
         # Try os.walk, but not so ideal as I expected
         # dir_list = []
@@ -129,17 +138,11 @@ class Diff:
         return dirs
 
     def _finds(self, d1, d2, ignore_space=True):
-        """
-        :param d1: Absolute path of first directories
-        :param d2: Absolute path of second directories
-        :param ignore_space: set True to ignore the space at the end of lines, default True
-        :return:
-        """
         dirs_1, dirs_2 = self._iterate(d1), self._iterate(d2)
         flag = False
         if dirs_1 != dirs_2:
             flag = True
-            self._log_output.append(self.NAME_DIFF)
+            self._log_output.append(self._NAME_DIFF)
             dif1, dif2 = dirs_1-dirs_2, dirs_2-dirs_1
             if dif1:
                 self._log_output.append(f"Only in {d1}:\n")
@@ -159,3 +162,11 @@ class Diff:
         return flag
 
 
+class NotTheRightTypeError(AssertionError):
+    def __init__(self, message):
+        self.message = message
+
+
+class CanNotFindPathError(AssertionError):
+    def __init__(self, message):
+        self.message = message
